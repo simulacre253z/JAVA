2539fc3a084d5ccf8deb1c1a39931953ebef04e6
Simulacre <63138369+simulacre253z@users.noreply.github.com>
2022-05-12 16:56:29 +0900

    Add files via upload

---
+++ VariableConstant.java
@@ -0,0 +1,23 @@
+package dataType;
+
+public class VariableConstant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final float PI = 3.14f;
+		final int MAX_NUM = 20;
+		
+		int age = 25;
+		String address = "충청남도 보령시 대천동";
+		final char gender = '남';
+		
+		System.out.println(PI);
+		System.out.println(MAX_NUM);
+		
+		System.out.println(age);
+		System.out.println(address);
+		System.out.println(gender);
+	}
+
+}
